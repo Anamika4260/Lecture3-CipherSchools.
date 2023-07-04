3d1cb42a206ea802825a1e3205c87e95121b1f00
Anamika4260 <104004726+Anamika4260@users.noreply.github.com>
2023-07-04 06:34:24 +0530

    Update lec3-1.cpp

--- lec3.cpp
+++ lec3.cpp
@@ -1,13 +1,15 @@
-#include<iostram>
-using namespace std;
-int main()
-{
-??declare 2 variables and then assign them values of , and then add them 
-int a;
-int b;
-a=3;
-b=5;
-int c;
-c=a+b;
-cout<<c;
-}
+
+got answers of these questions :
+1. what are variables and constant in c++\
+2. how will thw computer identify the kind of input we give 
+3. how will the computer store the input we give 
+
+
+*DATATYPES- it defines the type of variable which is declared.
+ * TYPES OF DATATYPES
+interger types: int,short ,long,unsigned int, etc
+floating-point types: float,double
+character type char,wchar_t
+boolean type: bool
+void type:void 
+user-defined:struct,class,enum,etc
